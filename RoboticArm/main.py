e8e1f7c71f6b8ecec628e3d2e2b5dfc12fa6f982
doug <doug@ehtter.com>
2018-08-25 11:21:34 -0700

    Removed Library code Update arm code

--- RoboticArm/main.py
+++ RoboticArm/main.py
@@ -4,7 +4,6 @@
 
 import math
 import sys
-#import spidev
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -26,16 +25,10 @@ from kivy.config import Config
 from kivy.core.window import Window
 from time import sleep
 import RPi.GPIO as GPIO 
-sys.path.insert(0, "Libraries/Hardware")
+sys.path.insert(0, "/home/pi/Libraries/Hardware")
 import Stepper 
-sys.path.insert(0, "Libraries/Hardware/RPiMIB")
+sys.path.insert(0, "/home/pi/Libraries/Hardware/RPiMIB")
 import RPiMIB
-
-#ramp.initGPIOState()
-#def initialize():
-#    toggleArm(UP)
-#    toggleMagnet(OFF)
-#    toggleRamp(TO_HOME)
 
 # ////////////////////////////////////////////////////////////////
 # //            DECLARE APP CLASS AND SCREENMANAGER             //
@@ -49,10 +42,9 @@ class MyApp(App):
         return sm
 
 Builder.load_file('main.kv')
-Builder.load_file('Libraries/Kivy/DPEAButton.kv')
-Builder.load_file('Libraries/Kivy/PauseScene.kv')
+Builder.load_file('/home/pi/Libraries/Kivy/DPEAButton.kv')
+Builder.load_file('/home/pi/Libraries/Kivy/PauseScene.kv')
 Window.clearcolor = (.1, .1,.1, 1) # (WHITE)
-Window.size = (1500, 800)
 
 RPiMIB.openSPI()
 
@@ -60,7 +52,7 @@ RPiMIB.openSPI()
 # //                    SLUSH/HARDWARE SETUP                    //
 # ////////////////////////////////////////////////////////////////
 
-arm = Stepper.Stepper(speed = 100)
+arm = Stepper.Stepper(port = 0, speed = 100)
 
 # ////////////////////////////////////////////////////////////////
 # //                      GLOBAL VARIABLES                      //
@@ -75,11 +67,8 @@ OFF = False
 YELLOW = .180, 0.188, 0.980, 1
 BLUE = 0.917, 0.796, 0.380, 1
 
-freq_clock = 16000000
-spiFrequency = 1000000
-lowerTowerPosition = 30
-upperTowerPosition = 40
-
+lowerTowerPosition = 14.5
+upperTowerPosition = 18.5
 
 # ////////////////////////////////////////////////////////////////
 # //                       MAIN FUNCTIONS                       //
@@ -102,8 +91,6 @@ class MainScreen(Screen):
     isMagnetOn = OFF
     isArmUp = UP
     
-
-    
     def toggleArm(self):
         self.ids.armControl.color = 0.701, 0.560, 0.078, 1
         self.isArmUp = not self.isArmUp
@@ -113,12 +100,12 @@ class MainScreen(Screen):
         RPiMIB.reset();
         if (isArmUp == UP):
             self.ids.armControl.text = "Raise Arm"
+            arm.setGPIOState(0, 7, 1)
             print('raise arm')
-            #arm.setIOState(0, 1, 1)
         else:
             self.ids.armControl.text = "Lower Arm"
+            arm.setGPIOState(0, 7, 0)
             print('lower arm')
-            #arm.setIOState(0, 1, 0)
         self.isArmUp = isArmUp
         
     def toggleMagnet(self):
@@ -127,29 +114,52 @@ class MainScreen(Screen):
         self.magnetControl(self.isMagnetOn)
         
     def magnetControl(self, isMagnetOn):
-        RPiMIB.reset();
         if (isMagnetOn == ON):
             self.ids.magnetControl.text = "Hold Ball"
             print('magnet on')
-           # RPiMIB.sendI2C(0x41, self.staircaseSpeed)
+            RPiMIB.sendPWM(5, 5000)
         else:
             self.ids.magnetControl.text = "Release Ball"
             print('magnet off')
-           # RPiMIB.sendI2C(0x41, 0)
+            RPiMIB.sendPWM(5, 0)
         self.isMagnetOn = isMagnetOn
     
     def auto(self):
         self.ids.auto.color = 0.180, 0.188, 0.980, 1
         self.ids.auto.text = "Stop"
-##        if (self.isBallAtBottomOfRamp()):
-##            self.turnOnStaircase(OFF)
-##            self.openGate(CLOSE)
-##            self.homeRamp(TOP)
-##            
-##        self.turnOnStaircase(ON)
-##        sleep(1)
-##        self.openGate(OPEN)
-        
+
+        self.armControl(UP)
+        self.magnetControl(ON)
+        self.homeArm()
+                
+        if (self.isBallOnTallTower()):
+            arm.setArmPosition(upperTowerPosition)
+            self.pickUpBall()
+            arm.setArmPosition(lowerTowerPosition)
+            self.dropBall()
+
+        if (self.isBallOnShortTower()):
+            arm.setArmPosition(lowerTowerPosition)
+            self.pickUpBall()
+            arm.setArmPosition(upperTowerPosition)
+            self.dropBall()
+
+        self.homeArm()
+
+    def pickUpBall():
+        self.armControl(DOWN)
+        sleep(0.25)
+        self.magnetControl(ON)
+        sleep(0.25)
+        self.armControl(UP)
+
+    def dropBall():
+        self.armControl(DOWN)
+        sleep(0.25)
+        self.magnetControl(OFF)
+        sleep(0.25)
+        self.armControl(UP)
+    
     def setArmPosition(self, position):
         self.armPosition = position
         self.ids.armControlLabel.text = "Arm Position " + str(math.trunc(self.armPosition))
@@ -159,10 +169,10 @@ class MainScreen(Screen):
         arm.home(0)
         
     def isBallOnTallTower(self):
-        return arm.getIOState(1, 1)
+        return arm.getGPIOState(1, 1)
 
     def isBallOnShortTower(self):
-        return arm.getIOState(0, 1)
+        return arm.getGPIOState(1, 2)
         
     def resetColors(self):
         self.ids.armControl.color = YELLOW
@@ -171,8 +181,6 @@ class MainScreen(Screen):
 
     def quit(self):
         quitAll()
-    # more functions here
-
     
 def signal_handler(signal, frame):
     print ('Goodbye')
@@ -185,8 +193,6 @@ sm.add_widget(MainScreen(name = 'main'))
 # ////////////////////////////////////////////////////////////////
 # //                          RUN APP                           //
 # ////////////////////////////////////////////////////////////////
-
-reset()
 
 MyApp().run()
 RPiMIB.closeSPI()
