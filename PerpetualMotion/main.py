30095d43b84a331e9adbbcdc9af30fb73b2fcdfc
Michelle Qin <qin.michelle24@gmail.com>
2018-10-16 08:48:48 -0700

    Oops, fix last commit to work properly

--- PerpetualMotion/main.py
+++ PerpetualMotion/main.py
@@ -1,9 +1,14 @@
+# Michelle Qin
+
 # ////////////////////////////////////////////////////////////////
 # //                     IMPORT STATEMENTS                      //
 # ////////////////////////////////////////////////////////////////
 
 import math
 import sys
+#sys.path.insert(1, "/home/pi/kivy")
+#import kivy
+#kivy.require("1.8.0")
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -21,16 +26,18 @@ from kivy.uix.behaviors import ButtonBehavior
 from kivy.clock import Clock
 from kivy.animation import Animation
 from functools import partial
-from kivy.config import Config
-from kivy.core.window import Window
 from time import sleep
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
 sys.path.insert(0, "/home/pi/Documents/RaspberryPiCommon/Libraries/Hardware")
 import Stepper 
 sys.path.insert(0, "/home/pi/Documents/RaspberryPiCommon/Libraries/Hardware/RPiMIB")
 import RPiMIB
+import threading
 
+#imports for sensor
+import Slush
 RPiMIB.openSPI()
+SlushEngine = Slush.sBoard()
 
 #Intialization
 ON = False
@@ -42,8 +49,11 @@ CLOSE = True
 YELLOW = .180, 0.188, 0.980, 1
 BLUE = 0.917, 0.796, 0.380, 1
 
-rampLength = 730
+#730 is the default length, but sometimes it only goes up 1/4 of the way up the ramp. In those cases, use 2950. If you
+#notice the ramp is crashing into the other end, switch to 730.
+rampLength = 2950 #730
 ramp = Stepper.Stepper(port = 0, speed = 100)
+ramp.home(0)
 
 
 # ////////////////////////////////////////////////////////////////
@@ -98,57 +108,141 @@ def quitAll():
 # ////////////////////////////////////////////////////////////////
 
 
+# "reliableSetPWM" accounts for discrepancies with inconsistent hardware - sometimes, the 1st signal doesn't get passed,
+# so this ensures that it gets passed if the 1st signal doesn't go through. Even if the 1st signal gets passed,
+# this will still work b/c the 2nd signal will quickly override it.
+def reliableSetPWM(port, PWM):
+    RPiMIB.sendPWM(port, PWM)
+    sleep(0.05) 
+    RPiMIB.sendPWM(port, PWM)
+
 class MainScreen(Screen):
     staircaseSpeedText = '0'
     rampSpeed = 100
     staircaseSpeed = 40
-    isGateOpen = CLOSE
-    isStaircaseOn = OFF
+    isGateOpen = False
+    isStaircaseOn = False
     isRampHome = HOME
-
+    submitRamp = False  
+    notRampSubmissionComplete = False
+    # numberOfCycles keeps track of how many cycles have passed (for "Start" process). Default # cycles = 0
+    numberOfCycles = 0
+    counter = 0
 
     #    self.ids.rampSpeedSlider.value = rampSpeed
     
+# "update" -------
+    def update(self, timeElapsed):
+        #pull ramp state
+        print('update')
+        if self.notRampSubmissionComplete:
+            # readSwitch returns true if the ramp is home
+            if (self.isRampHome == HOME) and ramp.readSwitch():
+                ramp.hardStop()
+                ramp.setAsHome()
+                self.notRampSubmissionComplete = False
+            # in Motor.py a relativeMove is done if the second bit of
+            # getStatus is set
+            elif (self.isRampHome == TOP) and \
+                    (ramp.getStatus() & 0x2) != 0:
+                self.notRampSubmissionComplete = False     
+
+# isGateOpen is the value for whether the gate is open or closed
+# When you press the "Open/Close Gate" button, it will call "toggleGate" (if you want to confirm, check main.kv).
+# "toggleGate" switches the value of isGateOpen and then calls "resubmitState" (check the explanation for that method
+# where it's defined). When you press the button, you want to change its current state (i.e. if the gate is currently
+# closed, you want it to be opened when you press the button & vice versa).
+# That's what switching the value of isGateOpen is for.
     def toggleGate(self):
         self.ids.gate.color = 0.701, 0.560, 0.078, 1
+        print("[debug]", self.isGateOpen)
         self.isGateOpen = not self.isGateOpen
-        self.openGate(self.isGateOpen)
+        #self.openGate(not self.isGateOpen)
+        #sleep(0.2)
+        #self.openGate(not self.isGateOpen)
+        #sleep(0.2)
+        #self.openGate(not self.isGateOpen)
+        self.resubmitState()
 
+# "openGate" is the function that actually opens/closes the gate.
     def openGate(self, isOpen):
-        if (isOpen == OPEN):
-            self.ids.gate.text = "Close Gate"
-            print('close gate')
-            RPiMIB.sendPWM(4, 2000)
-        else:
+        # if you want the gate to be closed, it will change the button to say "Open Gate"  (for next round)
+        # and send 500 PWM to port 4
+        # Note: 500 PWM is the amount to close the gate
+        if (isOpen == False):
             self.ids.gate.text = "Open Gate"
+            reliableSetPWM(4, 500)
+            #RPiMIB.sendPWM(4, 500)
+            print('close gate')
+        # if you want the gate to beopened, it will change the button to say "Close Gate"  (for next round)
+        # and send 4000 PWM to port 4
+        # Note: 4000 PWM is the amount to open the gate
+        else:
+            self.ids.gate.text = "Close Gate"
             print('open gate')
-            RPiMIB.sendPWM(4, 1000)
+            reliableSetPWM(4, 4000)
         self.isGateOpen = isOpen
-        
+
+# isStaircaseOn is the value for whether the staircase is on or off
+# When you press the "Staircase On/Off" button, it will call "toggleStaircase" (if you want to confirm, check main.kv).
+# "toggleStaircase" switches the value of isStaircaseOn and then calls "resubmitState" (check the explanation for
+# that method where it's defined). When you press the button, you want to change its current state (i.e. if  the
+# staircase is currently off, you want it to be on when you press the button & vice versa).
+# That's what switching the value of isStaircaseOn is for.
     def toggleStaircase(self):
         self.ids.staircase.color = 0.701, 0.560, 0.078, 1
-        self.isStaircaseOn = not self.isStaircaseOn
-        self.turnOnStaircase(self.isStaircaseOn)
-        
+        self.isStaircaseOn = not self.isStaircaseOn 
+        #self.turnOnStaircase(not self.isStaircaseOn)
+        #self.turnOnStaircase(not self.isStaircaseOn)
+        #self.turnOnStaircase(not self.isStaircaseOn)
+        self.resubmitState()
+
+# "turnOnStaircase" is the function that actually turns on/off the staircase.
     def turnOnStaircase(self, isOn):
-        if (isOn == True):
+        # if you want the staircase to be opened, it will change the button to say "Staircase Off"  (for next round)
+        # and send int(self.staircaseSpeed) * 125 PWM to port 5
+        # Note: int(self.staircaseSpeed) * 125 PWM is the default amount to turn on the staircase
+        if (isOn):
+            self.ids.staircase.text = "Staircase Off" 
             print('staircase on')
-            RPiMIB.sendPWM(5, 0)
+            #RPiMIB.sendPWM(5, 0)
+            reliableSetPWM(5, int(self.staircaseSpeed) * 125)
+        # if you want the staircase to be closed, it will change the button to say "Staircase On"  (for next round)
+        # and send 0 PWM to port 5
+        # Note: 0 PWM is the amount to turn off the staircase
         else: 
+            self.ids.staircase.text = "Staircase On" 
             print('staircase off')
-            RPiMIB.sendPWM(5, int(self.staircaseSpeed) * 100)
+            #RPiMIB.sendPWM(5, int(self.staircaseSpeed) * 125)
+            #RPiMIB.sendPWM(5, 1)
+            #sleep(0.2)
+            reliableSetPWM(5, 0)
         self.isStaircaseOn = isOn
-    
+
+# isRampHome is the value for whether the ramp is home or not
+# When you press the "Ramp Home/Top" button, it will call "toggleRamp" (if you want to confirm, check main.kv).
+# "toggleRamp" switches the value of isRampHome and then calls "resubmitState" (check the explanation for
+# that method where it's defined). When you press the button, you want to change its current state (i.e. if  the
+# ramp is currently home, you want it to go to the top when you press the button & vice versa).
+# That's what switching the value of isRampHome is for.
     def toggleRamp(self):
         self.ids.ramp.color = 0.701, 0.560, 0.078, 1
         self.isRampHome = not self.isRampHome
-        self.homeRamp(self.isRampHome)
-        
+        #self.homeRamp(not self.isRampHome)
+        self.submitRamp = True
+        self.resubmitState()
+
+# "homeRamp" is the function that actually brings the ramp home or top.
     def homeRamp(self, isHome):
+        # if you want the ramp to go home, it will change the button to say "Ramp To Top"  (for next round)
+        # and --------- (not sure what ramp.run does??
+        # Note:
         if (isHome == HOME):
             self.ids.ramp.text = "Ramp To Top"
             print('ramp home') 
-            ramp.home(1)
+            ramp.run(0, ramp.speed)
+        # if you want the ramp to go to the top, it will change the button to say "Ramp Home"  (for next round)
+        # and tell the ramp to move to the top along the rampLength (defined near the top of this file).
         else:
             self.ids.ramp.text = "Ramp Home"
             # If PB0 is high
@@ -163,67 +257,133 @@ class MainScreen(Screen):
 #            print(ramp.getIOState(1, 0))
 
 #            if (ramp.getIOState(1, 0) == 0):
-            ramp.relativeMove(-rampLength)
-            print('ramp done');
-               
+#            ramp.relativeMove(rampLength)
+            ramp.relativeMove(rampLength)           
+            print('ramp done');              
         self.isRampHome = isHome
+        #self.turnOnStaircase(self.isStaircaseOn)
         
     def isBallAtBottomOfRamp(self):
         return ramp.getIOState(1, 1)
     
+#    def start(self):
+#        while(self.numberOfCycles < 5):
+#            if (SlushEngine.getIOState(0, 0) == False):
+#                self.auto()
+#            self.numberOfCycles = self.numberOfCycles + 1
+#        self.ids.auto.text = "Start"
+
+# When you press the "Start" button, it will call "auto" (if you want to confirm, check main.kv)
+# The Start process essentially combines all the separate components (ramp, staircase, gate) to get the ball across
+# the sculpture & fulfill its name of the "Perpetual Motion Machine"
+# Description: First, the ramp will bring the ball to the top and to the staircase; staircase will turn on to
+# bring ball to gate; gate will open to let ball through and back to the ramp. The sensor at the home of the ramp
+# detects the ball and starts the entire process again
     def auto(self):
+        # changes "Start" button to say "Stop" when it's running
         self.ids.auto.text = "Stop"
         self.ids.auto.color = 0.180, 0.188, 0.980, 1
-        
+
+        # initializes each component (check explanation of this function where it's defined)
         self.initialize()
         sleep(1)
-        #if (self.isBallAtBottomOfRamp()):
+            #if (SlushEngine.getIOState(j, i) == 
+            #if (self.isBallAtBottomOfRamp()):
+
+        # First, the ramp brings the ball to the top
         self.homeRamp(TOP)
-        sleep(1)
-        
-        self.turnOnStaircase(ON)
+
+        # After bringing the ball to the top, the ramp will return home
         self.homeRamp(HOME)
 
-        self.turnOnStaircase(OFF)
-        sleep(0.5)
-        self.openGate(OPEN)
+        self.turnOnStaircase(False)
+            
+        sleep(1)
+
+        # While ramp is going back home, the staircase turns on to bring ball to gate
+        self.turnOnStaircase(True)
+        sleep(5)
+        #gate opens to let the ball through
+        self.openGate(True)
         sleep(1)
         self.openGate(CLOSE)
-        self.ids.auto.text = "Start"
-        
+        self.turnOnStaircase(False)
+        self.openGate(False)
+            #self.ids.auto.text = "Start"
+        sleep(2)
+
+        # the sensor: 1st parameter represents Board (A); 2nd parameter represents port (0)
+        # When SlushEngine(Board, port) = False, that means there's an object (ball) in front of it
+        if (SlushEngine.getIOState(0, 0) == False):
+            # after the ball comes to the sensor/home of ramp, a cycle will pass (increment by 1)
+            self.numberOfCycles = self.numberOfCycles + 1
+            # NOTE: 3 is the number of cycles to run "perpetually". This can be changed in the code. I'm  trying
+            # to add a slider on the user interface so the user can decide how many cycles to run. But, when I tried to
+            # do that, I got many errors (something to do with kivy I think). Will work on this later.
+            if(self.numberOfCycles < 3):
+                #If 3 cycles haven't passed, it will start the process again by calling auto()
+                self.auto()
+            # once 3 cycles have passed and the process is done, will change the button back to "Start"
+            self.ids.auto.text = "Start"
+
+    # main.kv calls "setRampSpeed" when changes are made on the slider for the ramp speed
+    # When slider is changed to a new value, "setRampSpeed" will set the ramp speed to that new value
     def setRampSpeed(self, speed):
         self.rampSpeed = speed
         self.ids.rampSpeedLabel.text = "Ramp Speed: " + str(math.trunc(self.rampSpeed))
-        print("Setting ramp speed: " + str(self.rampSpeed))
+        print("Setting ramp speed: " + str(speed))
         ramp.setSpeed(self.rampSpeed)
-        
+
+    # main.kv calls "setStaircaseSpeed" when changes are made on the slider for the staircase speed
+    # When slider is changed to a new value, "setStaircaseSpeed" will set the staircase speed to that new value
     def setStaircaseSpeed(self, speed):
         self.staircaseSpeed = speed
         self.ids.staircaseSpeedLabel.text = "Staircase Speed: " + str(math.trunc(self.staircaseSpeed))
         print("Setting staircase speed: " + str(speed))
+
+#    def setNumberOfCycles(self, number):
+#        self.numberOfCycles = number
+#        self.ids.numberOfCyclesLabel.text = "Number of Cycles: " + str(math.trunc(self.numberOfCycles))
+#        print("Setting number of cycles: " + str(number))
         
     def isBallAtBottomOfRamp(self):
         return ramp.getIOState(1, 4)
 
     def isBallAtTopOfRamp(self):
         return ramp.getIOState(1, 5)
-        
+
     def resetColors(self):
         self.ids.gate.color = YELLOW
         self.ids.staircase.color = YELLOW
         self.ids.ramp.color = YELLOW
         self.ids.auto.color = BLUE
-    
-    def initialize(self):
-        self.openGate(CLOSE)
-        self.turnOnStaircase(OFF)
-        self.homeRamp(HOME)
 
+    # Will initialize all components - i.e. gate will close, ramp will go home, staircase will turn off (assuming these
+    # components are not yet initialized; otherwise, nothing will happen.
+    def initialize(self):
+        self.isGateOpen = False
+        self.isRampHome = HOME
+        self.isStaircaseOn = False
+        # The following 3 lines of code were implemented after the ramp was not cooperating
+        self.submitRamp = False  
+        self.notRampSubmissionComplete = False
+        self.resubmitState()
+        
     def exitProgram(self):
         quitAll()
 
-#    def __init__(self, **kwargs):
-#        self.initialize();
+    # This "init" was overridden so that right when the program runs, all the components will initialize
+    def __init__(self, **kwargs):
+        super(Screen,self).__init__(**kwargs)
+        self.initialize()
+        print("Stopyjkhhkh")
+        
+    def resubmitState(self):
+        if self.submitRamp:
+            self.homeRamp(self.isRampHome)
+            self.submitRamp = False
+        self.turnOnStaircase(self.isStaircaseOn)
+        self.openGate(self.isGateOpen)
 
 class PauseScene(Screen):
     pass
@@ -245,7 +405,12 @@ def signal_handler(signal, frame):
     quit()
     sys.exit(0)
 
-sm.add_widget(MainScreen(name = 'main'))
+screen = MainScreen(name = 'main')
+
+#implemented..
+Clock.schedule_interval(screen.update, 1.0/30.0)
+
+sm.add_widget(screen)
 sm.add_widget(PauseScene(name = 'pauseScene'))
 
 # ////////////////////////////////////////////////////////////////
